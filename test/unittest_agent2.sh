853456204e702d4e360ad8b0ea82b686c3e80134
Peter L Chang (RD-TW) <peterl_chang@trend.com.tw>
2015-12-22 10:01:16 +0800

    convert coverage report from xml to html

--- test/unittest_agent2.sh
+++ test/unittest_agent2.sh
@@ -4,5 +4,5 @@ cp ${PWD}/bin/agent2.py /tmp/agent2_test
 cp ${PWD}/test/unittest/unittest_agent2.py /tmp/agent2_test
 mkdir -p /tmp/agent2_test/report
 nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/agent2_unit_result.xml --cover-erase --with-coverage --cover-package=agent2 -w /tmp/agent2_test/ unittest_agent2.py
-coverage xml -o $CIRCLE_TEST_REPORTS/agent2_coverage.xml /tmp/agent2_test/agent2.py
+coverage html -o $CIRCLE_TEST_REPORTS/agent2_coverage.xml /tmp/agent2_test/agent2.py
 
