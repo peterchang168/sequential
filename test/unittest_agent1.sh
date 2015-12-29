1274f2617555f879b5699cf146de9e9cee58032e
Peter L Chang (RD-TW) <peterl_chang@trend.com.tw>
2015-12-29 19:33:51 +0800

    modify JUNIT XML path

--- test/unittest_agent1.sh
+++ test/unittest_agent1.sh
@@ -5,5 +5,5 @@ cp ${PWD}/bin/agent1.py /tmp/agent1_test
 cp ${PWD}/test/unittest/unittest_agent1.py /tmp/agent1_test
 mkdir -p /tmp/agent1_test/report
 nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/agent1_unit_result.xml --cover-erase --with-coverage --cover-package=agent1 -w /tmp/agent1_test/ unittest_agent1.py
-coverage xml -o $CIRCLE_TEST_REPORTS/agent1_coverage.xml /tmp/agent1_test/agent1.py
+coverage xml -o $CIRCLE_TEST_REPORTS/junit/agent1_coverage.xml /tmp/agent1_test/agent1.py
 coverage html -d $CIRCLE_ARTIFACTS/coverage
