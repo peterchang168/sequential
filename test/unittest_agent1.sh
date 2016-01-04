81e28d446c9adcd18dcd969677937b73da39f751
Peter L Chang (RD-TW) <peterl_chang@trend.com.tw>
2016-01-04 11:50:28 +0800

    generate unittest_coverage.html through junit-viewer

--- test/unittest_agent1.sh
+++ test/unittest_agent1.sh
@@ -6,6 +6,7 @@ cp ${PWD}/test/unittest/unittest_agent1.py /tmp/agent1_test
 mkdir -p /tmp/agent1_test/report
 mkdir $CIRCLE_TEST_REPORTS/junit/
 nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/junit/agent1_unit_result.xml --cover-erase --with-coverage --cover-package=agent1 -w /tmp/agent1_test/ unittest_agent1.py
-junit-viewer --results=$CIRCLE_TEST_REPORTS/junit/ --save=$CIRCLE_TEST_REPORTS/agent1_unit_result.html
+junit-viewer --results=$CIRCLE_TEST_REPORTS/junit/agent1_unit_result.xml --save=$CIRCLE_TEST_REPORTS/agent1_unit_result.html
 coverage xml -o $CIRCLE_TEST_REPORTS/junit/agent1_coverage.xml /tmp/agent1_test/agent1.py
+junit-viewer --results=$CIRCLE_TEST_REPORTS/junit/agent1_coverage.xml --save=$CIRCLE_TEST_REPORTS/agent1_coverage.html
 coverage html -d $CIRCLE_ARTIFACTS/coverage
